52e52929c76ec4623b7266a0235b1e7b8d831c99
Villalobos <luizf.villalobos@gmail.com>
2020-09-16 17:30:38 -0300

    DEBUG: Change to documentation to debug read the docs 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Luiz Frederico Villalobos'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
