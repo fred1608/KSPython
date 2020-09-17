8c62575b1c3f4252f1d93b02f2745fb31e2e70e2
Villalobos <luizf.villalobos@gmail.com>
2020-09-17 10:54:51 -0300

    DEBUG: Change to documentation to debug read the docs 4

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,8 +46,13 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'alabaster'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/intro.rst
+++ docs/intro.rst
@@ -4,4 +4,7 @@ Introduction
 
 This is the documentation of a library called KSPython, made to help people design Kerbal Space Program rockets. It features quick to use syntax for prototyping and simulations. 
 
-More details about the library can be found at https://github.com/fred1608/KSPython.
+More details about the library, including examples, can be found at https://github.com/fred1608/KSPython.
+
+
+
